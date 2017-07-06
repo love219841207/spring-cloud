0f6b476931e6e67aba0b7fe8fe9936e040ab8fe2
dongxu <dong.xu@99wuxian.com>
2017-07-06 14:42:30 +0800

    init

--- cloud-user-service/src/main/java/com/think/cloud/service/user/ServiceUserApplication.java
+++ cloud-user-service/src/main/java/com/think/cloud/service/user/ServiceUserApplication.java
@@ -4,6 +4,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
@@ -11,6 +12,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 @SpringBootApplication
 @EnableSwagger2
 @MapperScan(basePackages = "com.think.cloud.service.user.mapper")
+@RefreshScope
 public class ServiceUserApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ServiceUserApplication.class, args);
